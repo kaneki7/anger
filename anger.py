589af5abf112bb8e9027fe988a7cd319bed090d9
kaneki7 <honeyahmed2000@gmail.com>
2017-08-15 17:13:54 -0700

    Add files via upload

---
+++ anger.py
@@ -0,0 +1,119 @@
+
+
+print("Would you like to start?")
+
+user_input = input()
+while user_input != "yes":
+    user_input = input()
+    print("Type 'yes' when you are ready.")
+print("you simply couldn't take it anymore, the pressure put on you by your parents' high expectations. They expect you to get perfect grades and make friends with people who couldn't come close to understanding you. Constantly comparing you to to others and telling you to never end up like those people, those people  who've wasted their lives away. They treat your passions as a joke, something you do for fun that holds no actual value.")
+print("You had endured this for years, thinking you would be free once you graduated but after today that looked to be an unfulfilled fantasy. Besides you had finally found friends your parents accepted. You had been in your biology class enjoying a documentary on human anatomy when the intercom beeped, indicating a message was about to be delivered, the teacher tried to get the class to quiet down but that of course failed miserably.")
+print ("the message could barely be heard but,'_____ , please report to the principle's office I repeat ______ please report to  the principle's office.' the whole class looked to you with questioning gazes. You carefully schooled your expression and made your way out of the classroom and toward the principal's office. you politely knocked on the door and a voice rang out,'please come in _____________'. You were not prepared to see your parents behind the door shaking hands with the principle. Your parents smiled brightly at you,'we have wonderful news _____, you're being transferred schools.' You stared at them with a frozen expression and everything turned black and you slipped into unconsciousness.")
+print("The first thing you hear is a rush of wind that whipped around you. You cracked your eyes slowly, your vision was blurred regretted it immediately. You were a 1,000 feet in the air, you grew dizzy from the height so instead you looked up to see a giant bird if it could be called that. It had the body, tail, and back legs of a lion; the head and wings of an eagle; and an eagle's talons as its front feet. fur covered its body, black in color, while it's wings and feathers were mottled white. It's blazing red eyes peered down at your small form and made a sound of amusement. do you want to ask it where it's taking you?")
+
+user_input = input()
+if user_input == "yes":
+    print("The Griffin ignored you and kept on it's course until in the disance a cliff could be seen.The Griffin descended into an opening at the highest point of the cliff.The Griffin released his grip on you, you flew throught the air, skidding across the cave floor and scraping your arms as you went.")
+    print("You groaned in pain as you stood up to face the Griffin.")
+    print("do you want to give the Griffin a piece of your mind?")
+
+    if user_input == "yes":
+        user_input = input()
+        print("Anger bubbled in you, anger that you had kept at bay till now. This bird had no idea who he was dealing with.'hey! who do you think you are lugging me around for hours and then throwing me inside this cave?!' the bird payed you no mind too busy getting itself comfortable inside it's home.'Calm yourself little human, I apologize for being a bit rough with you but I had to get you to my home so you can start.' ")
+        print("Your anger faded to give way to confusion.'start what?' the bird took out a mirror as long and twice as wide as you. The griffin's glowing red eyes bore into your own. 'your journey begins here,little human.' that was all he said before pushing you into the mirror. You cried out in fear but instead of being pierced by sharp glass, you passed through it.you blacked out.")
+        print("When you woke up you were falling.again.Panick set in and you screamed,arms flailing around you but it seemed luck was on your side because you landed on something.")
+        print("you opened your eyes to see the cloud you had moments ago been on in front of you.do you want to thank the cloud?")
+
+        if user_input == "no":
+            print("The Griffin bent his head,'I apologize for being a little on you,little human. Forgive me but I had to get you here so you could begin your journey.'")
+            print("You cock your head in confusion,'journey? What journey? the griffin's glowing red eyes softened.'Do not worry,little human.You will find the thing you seek'")
+            print("'what? what do you mean by the thing i've been seeking?' You were completely lost now.The Griffin said nothing in response. Instead he pulled out a mirror from behind him, a simple black picture framed vintage mirror.")
+            print("the griffin gestured toward the mirror. When you didn't budge he pierced his talon through the mirror.You took a step back preparing for the mirror to shatter into litte pieces.")
+            print("But that's not what happened.the griffin's talon simply passed through the mirror causing a ripple affect, similar to when something hits water.")
+            print("You look up at the Griffin for assureness and recieved a nod in return.")
+            print("You hesitantly took steps toward the mirror til you stood infront of it.")
+            print("do you want to go through it?")
+
+            if user_input == "yes":
+                print("you passed through the mirror")
+
+
+
+
+        user_input = input()
+    if user_input == "yes":
+        print("you smile gratefully at the kind cloud,'thank you for saving me.'")
+        print("it flies around your form before rising higher and higher to return to it's position.")
+        print("The sight before you takes your breath away. Desert of every type,shape,color and flavor formed the scenery.")
+        print("Candy cane trees and chocolate chip cookies that swam in a river of milk that ran into the distance.")
+        print("in front of you lay a fork in the path.to your right a Hot chocolate springs lay while to you left lay mountains made of Ice cream.")
+        print("would you like to go to Ice cream mountain?")
+    if user_input == "no":
+        print("you take the right path toward the hot springs.Aforce throws you into the hot springs.")
+        print("Game over")
+
+
+
+
+
+
+user_input = input()
+if user_input == "yes":
+    print("the path not a smooth one it seemed to grow colder with every step you took.Slowly snow began to fall innocently and you paused to admire the individual snowflakes eyes filled with wonder.")
+    print("You held out your hand and a single snowflake drifted gently onto your open palm and melted when it came in contact with your body heat. You continued on the path but as more time passed snow fell at a faster rate until you were ankle deep in snow.")
+    print("You were thankful that you remembered to wear a jacket in the morning.Your face had long since numbed to the point that you could not feel it.Your eyelids grew heavy to the that you could barely blink.")
+    print("You took the last few steps to the base of the mountain sighing in relief.")
+    print("In front of you stood a sign that read 'Welcome young traveler this mountain before you promises treasure beyond your wildest dreams but there is no reward without risk.You will go through trials that will test you in different ways.")
+    print("'do you want to enter the mountain?'")
+
+    user_input = input()
+    if user_input == "no":
+        print("the only thing that could be heard was your breath. suddenly the ground beneath you started shaking and you struggled to keep your balance")
+        print("the shaking stopped abruptly and you let out a breath of relief.The sign that had been ifront of you began to spin hectically before disappearing beneath the snow.")
+        print("the sign appeared underneath your feet and you were catapulted in the air and straight through an opening in the mouantain base.")
+
+
+if user_input == "yes":
+    print("you walk cautiously toward the mountain.")
+    print("do you want to look around you for a way inside?")
+    print("you look around you but stop when you see a small opening in the base of the mountian.")
+    print("do you want to go inside?")
+
+user_input = input()
+if user_input == "yes":
+    print("Only the crunch of snow beneath your feet was heard as you made your way to the opening.You get down on your knees and shuffle through the hole that was just big enough to get through.")
+    print("It gradually grew warmer as you went farther in.")
+    print("you hit the floor and rolled to a stop.When you looked up, you were face-to-face with a chimera. you scrambled to your feet and backed up until you hit the wall.Do you want to look around you?")
+    print("You were inside a ring. Like those boxing rings you'd seen on tv.Outside the ring there was an audience.Each audience member held a poster with encouraging words written on them.")
+    print("There was no one else inside the ring except you and the chimera.It was a lion, with the head of a goat arising from its back, and a tail that might end with a snake's head.It snorted out flames.")
+    print("A shiver of fear runs down your spine, your breathing comes out shaky and you have to clench your fists to keep them from trembling.")
+    print("'Welcome!Today you will fight this chimera in the ring.You will have weapons to choose from.There will be no help from anyone else.' a bodiless voice rings throughout the entire room.")
+    print("'choose any one of these weapons to defeat the chimera: a sword or a shield?'")
+    print("Type yes if you want a sword.Type no for a shield.")
+
+user_input = input()
+if user_input == "yes":
+    print("a sword apears in your hand.You look down at the sword and then back up at the 10 foot fire breathing chimera.Would you like to begin?")
+
+user_input = input()
+if user_input == "yes":
+    print("Taking a moment to control your breathing, you tightened your grip on the sword and stared the chimera down.You ran to the back of the chimera and saw it's snake tail.")
+    print("It was a 5 feet long rattlesnake.You slashed it with your sword, causing it's head to fly off it's body.The chimera cried out in pain and anger.")
+    print("The chimera turned around to face you, it's jaws wide open and a burst of flame headed right for you. You rolled to the side to avoid it.")
+    print("do you want to finish this fight?")
+
+user_input = input()
+if user_input == "no":
+    print("A shield appears in your hand.You look down at the shield and then back up at the 10 foot fire breathing chimera.Would you like to begin?")
+    print("the chimera charged, breaking through your armor.")
+    print("Game over")
+if user_input == "yes":
+    print("Taking a moment to control your breathing, you tightened your grip on the sword and stared the chimera down.You ran to the back of the chimera and saw it's snake tail.")
+    print("It was a 5 feet long rattlesnake.You slashed it with your sword, causing it's head to fly off it's body.The chimera cried out in pain and anger.")
+    print("The chimera turned around to face you, it's jaws wide open and a burst of flame headed right for you. You rolled to the side to avoid it.")
+    print("do you want to finish this fight?")
+
+    if user_input == "yes":
+        print("You gathered your energy, building it up and released into a final slash that lit up the whole room.Black was all you could see for a while.")
+        print("Your eyes cracked open and saw your parents faces.Their faces showed concern,'honey are you okay?'. You jumped into their arms.'mom.Dad I have something I have to tell you' You told your parent about your feelings about them and their involvement in your life.")
+        print("congragulations")
